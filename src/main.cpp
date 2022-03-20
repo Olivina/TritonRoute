901c20d84a4faa38c1186e64e1d8a08204e24c9e
Huang Yong <384953690@qq.com>
2022-03-20 19:32:09 +0800

    update usage

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 /*
  * Copyright (c) 2019, The Regents of the University of California
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *     * Redistributions of source code must retain the above copyright
@@ -13,12 +13,12 @@
  *     * Neither the name of the University nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE REGENTS BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
  * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
  * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
@@ -26,52 +26,99 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <cstring>
-#include <string>
 #include <chrono>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
 #include "FlexRoute.h"
 
 using namespace std;
 using namespace fr;
 
-int readParams(const string &fileName) {
+int readParams(const string& fileName)
+{
   int readParamCnt = 0;
   fstream fin(fileName.c_str());
   string line;
-  if (fin.is_open()){
-    while (fin.good()){
+  if (fin.is_open()) {
+    while (fin.good()) {
       getline(fin, line);
-      if (line[0] != '#'){
-        char delimiter=':';
+      if (line[0] != '#') {
+        char delimiter = ':';
         int pos = line.find(delimiter);
         string field = line.substr(0, pos);
         string value = line.substr(pos + 1);
         stringstream ss(value);
-        if (field == "lef")           { LEF_FILE = value; ++readParamCnt;}
-        else if (field == "def")      { DEF_FILE = value; REF_OUT_FILE = DEF_FILE; ++readParamCnt;}
-        else if (field == "guide")    { GUIDE_FILE = value; ++readParamCnt;}
-        else if (field == "outputTA") { OUTTA_FILE = value; ++readParamCnt;}
-        else if (field == "output")   { OUT_FILE = value; ++readParamCnt;}
-        else if (field == "outputguide") { OUTGUIDE_FILE = value; ++readParamCnt;}
-        else if (field == "outputMaze") { OUT_MAZE_FILE = value; ++readParamCnt;}
-        else if (field == "outputDRC") { DRC_RPT_FILE = value; ++readParamCnt;}
-        else if (field == "threads")  { MAX_THREADS = atoi(value.c_str()); ++readParamCnt;}
-        else if (field == "verbose")    VERBOSE = atoi(value.c_str());
-        else if (field == "dbProcessNode") { DBPROCESSNODE = value; ++readParamCnt;}
-        else if (field == "drouteOnGridOnlyPrefWireBottomLayerNum") { ONGRIDONLY_WIRE_PREF_BOTTOMLAYERNUM = atoi(value.c_str()); ++readParamCnt;}
-        else if (field == "drouteOnGridOnlyPrefWireTopLayerNum") { ONGRIDONLY_WIRE_PREF_TOPLAYERNUM = atoi(value.c_str()); ++readParamCnt;}
-        else if (field == "drouteOnGridOnlyNonPrefWireBottomLayerNum") { ONGRIDONLY_WIRE_NONPREF_BOTTOMLAYERNUM = atoi(value.c_str()); ++readParamCnt;}
-        else if (field == "drouteOnGridOnlyNonPrefWireTopLayerNum") { ONGRIDONLY_WIRE_NONPREF_TOPLAYERNUM = atoi(value.c_str()); ++readParamCnt;}
-        else if (field == "drouteOnGridOnlyViaBottomLayerNum") { ONGRIDONLY_VIA_BOTTOMLAYERNUM = atoi(value.c_str()); ++readParamCnt;}
-        else if (field == "drouteOnGridOnlyViaTopLayerNum") { ONGRIDONLY_VIA_TOPLAYERNUM = atoi(value.c_str()); ++readParamCnt;}
-        else if (field == "drouteViaInPinBottomLayerNum") { VIAINPIN_BOTTOMLAYERNUM = atoi(value.c_str()); ++readParamCnt;}
-        else if (field == "drouteViaInPinTopLayerNum") { VIAINPIN_TOPLAYERNUM = atoi(value.c_str()); ++readParamCnt;}
-        else if (field == "drouteEndIterNum") { END_ITERATION = atoi(value.c_str()); ++readParamCnt;}
-        else if (field == "OR_SEED") {OR_SEED = atoi(value.c_str()); ++readParamCnt;}
-        else if (field == "OR_K") {OR_K = atof(value.c_str()); ++readParamCnt;}
+        if (field == "lef") {
+          LEF_FILE = value;
+          ++readParamCnt;
+        } else if (field == "def") {
+          DEF_FILE = value;
+          REF_OUT_FILE = DEF_FILE;
+          ++readParamCnt;
+        } else if (field == "guide") {
+          GUIDE_FILE = value;
+          ++readParamCnt;
+        } else if (field == "outputTA") {
+          OUTTA_FILE = value;
+          ++readParamCnt;
+        } else if (field == "output") {
+          OUT_FILE = value;
+          ++readParamCnt;
+        } else if (field == "outputguide") {
+          OUTGUIDE_FILE = value;
+          ++readParamCnt;
+        } else if (field == "outputMaze") {
+          OUT_MAZE_FILE = value;
+          ++readParamCnt;
+        } else if (field == "outputDRC") {
+          DRC_RPT_FILE = value;
+          ++readParamCnt;
+        } else if (field == "threads") {
+          MAX_THREADS = atoi(value.c_str());
+          ++readParamCnt;
+        } else if (field == "verbose")
+          VERBOSE = atoi(value.c_str());
+        else if (field == "dbProcessNode") {
+          DBPROCESSNODE = value;
+          ++readParamCnt;
+        } else if (field == "drouteOnGridOnlyPrefWireBottomLayerNum") {
+          ONGRIDONLY_WIRE_PREF_BOTTOMLAYERNUM = atoi(value.c_str());
+          ++readParamCnt;
+        } else if (field == "drouteOnGridOnlyPrefWireTopLayerNum") {
+          ONGRIDONLY_WIRE_PREF_TOPLAYERNUM = atoi(value.c_str());
+          ++readParamCnt;
+        } else if (field == "drouteOnGridOnlyNonPrefWireBottomLayerNum") {
+          ONGRIDONLY_WIRE_NONPREF_BOTTOMLAYERNUM = atoi(value.c_str());
+          ++readParamCnt;
+        } else if (field == "drouteOnGridOnlyNonPrefWireTopLayerNum") {
+          ONGRIDONLY_WIRE_NONPREF_TOPLAYERNUM = atoi(value.c_str());
+          ++readParamCnt;
+        } else if (field == "drouteOnGridOnlyViaBottomLayerNum") {
+          ONGRIDONLY_VIA_BOTTOMLAYERNUM = atoi(value.c_str());
+          ++readParamCnt;
+        } else if (field == "drouteOnGridOnlyViaTopLayerNum") {
+          ONGRIDONLY_VIA_TOPLAYERNUM = atoi(value.c_str());
+          ++readParamCnt;
+        } else if (field == "drouteViaInPinBottomLayerNum") {
+          VIAINPIN_BOTTOMLAYERNUM = atoi(value.c_str());
+          ++readParamCnt;
+        } else if (field == "drouteViaInPinTopLayerNum") {
+          VIAINPIN_TOPLAYERNUM = atoi(value.c_str());
+          ++readParamCnt;
+        } else if (field == "drouteEndIterNum") {
+          END_ITERATION = atoi(value.c_str());
+          ++readParamCnt;
+        } else if (field == "OR_SEED") {
+          OR_SEED = atoi(value.c_str());
+          ++readParamCnt;
+        } else if (field == "OR_K") {
+          OR_K = atof(value.c_str());
+          ++readParamCnt;
+        }
       }
     }
     fin.close();
@@ -83,18 +130,24 @@ int readParams(const string &fileName) {
   }
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char** argv)
+{
   using namespace std::chrono;
   high_resolution_clock::time_point t1 = high_resolution_clock::now();
   if (argc == 1) {
-    cout <<"usage:\n ./TritonRoute \n  -lef <LEF_FILE> \n  -def <DEF_FILE> \n  -guide <GUIDE_FILE> \n  -output <OUTPUT_DEF> \n  [optional]-threads <NUM_THREADS>\n  [optional]-verbose" <<endl;
+    cout << "usage:\n ./TritonRoute \n  -lef <LEF_FILE> \n  -def <DEF_FILE> \n "
+            " -guide <GUIDE_FILE> \n  -output <OUTPUT_DEF> \n  "
+            "[optional]-threads <NUM_THREADS>\n  [optional]-verbose "
+            "<LOG_LEVEL>\n"
+            "LOG_LEVEL = {0, 1, 2}\n"
+         << endl;
     return 2;
   }
 
   if (argc == 2) {
     int readSuccess = readParams(string(argv[1]));
     if (readSuccess) {
-      cout <<"Error reading param file!!!" <<endl;
+      cout << "Error reading param file!!!" << endl;
       return 2;
     }
   } else {
@@ -109,7 +162,7 @@ int main(int argc, char** argv) {
         argv++;
         argc--;
         DEF_FILE = *argv;
-        REF_OUT_FILE = DEF_FILE; 
+        REF_OUT_FILE = DEF_FILE;
       } else if (strcmp(*argv, "-guide") == 0) {
         argv++;
         argc--;
@@ -127,20 +180,20 @@ int main(int argc, char** argv) {
         argc--;
         VERBOSE = atoi(*argv);
       } else {
-        cout <<"ERROR: Illegal command line option: " <<*argv <<endl;
+        cout << "ERROR: Illegal command line option: " << *argv << endl;
         return 2;
       }
       argv++;
     }
   }
-  
+
   FlexRoute router;
   router.main();
-  
+
   high_resolution_clock::time_point t2 = high_resolution_clock::now();
   duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
   if (VERBOSE > 0) {
-    cout <<endl <<"Runtime taken (hrt): " << time_span.count()    <<endl;
+    cout << endl << "Runtime taken (hrt): " << time_span.count() << endl;
   }
   return 0;
 }
